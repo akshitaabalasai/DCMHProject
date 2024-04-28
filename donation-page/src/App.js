b3e97aec58fc3f9d9a47f60fcd8bda5bf7e51263
Sohum Tiwary <sohum.tiwary@gmail.com>
2024-04-28 11:02:14 -0700

    final commits!

--- donation-page/src/App.js
+++ donation-page/src/App.js
@@ -1,12 +1,16 @@
 import DonationForm from './components/DonationForm';
 import HelpUs from './components/HelpUs';
 import classes from './App.module.css';
+import myImage from '../src/img.jpeg'
 
 function App() {
   return (
     <main className={classes.App}>
       <HelpUs />
       <DonationForm />
+      <div>
+      <img src={myImage} alt="Description" width="100" height="100" className="my-custom-class" />
+    </div>
     </main>
   );
 }
--- donation-page/src/components/HelpUs/index.jsx
+++ donation-page/src/components/HelpUs/index.jsx
@@ -6,9 +6,7 @@ const HelpUs = () => {
     <section className={classes.HelpUs}>
       <HelpUsTitle />
       <p className={classes.HelpUsParagraph}>
-        We'll get right to the point: we're asking you to support us. We're a
-        nonprofit that relies on support from people like you. If everyone
-        reading this gives $10 monthly, we can thrive for years.
+        "Davis Community Meals and Housing (DCMH) opened its doors in 1990 as a non-profit, non-denominational volunteer organization serving a free weekly meal to people in and around Davis who were homeless or without financial resources."
       </p>
     </section>
   );
